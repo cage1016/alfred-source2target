3e83c96db5f71f073c5e967c4060fc3b0956c1a2
Kai-Chu Chung <cage.chung@gmail.com>
2022-12-02 11:07:41 +0800

    project init

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2022 KAI CHU CHUNG
+*/
+package main
+
+import (
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/cage1016/alfred-targets2here/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
