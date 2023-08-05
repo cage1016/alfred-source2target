2ba28dff14788291a74af6bc9db28d899c024ced
Kai-Chu Chung <cage.chung@gmail.com>
2023-08-05 18:13:16 +0800

    feat(lib): add range select

---
+++ lib/lrange_test.go
@@ -0,0 +1,158 @@
+package lib
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPageRangeRegex(t *testing.T) {
+
+	type fields struct {
+		c *regexp.Regexp
+	}
+
+	type args struct {
+		n map[string]string
+	}
+
+	tests := []struct {
+		name    string
+		prepare func(f *fields)
+		args    args
+	}{
+		{
+			name: "test",
+			prepare: func(f *fields) {
+				f.c = PageRangeRegex
+			},
+			args: args{
+				n: map[string]string{
+					"#3-4,5-9,-5":                      "#3-4,5-9,-5",
+					"#3-4,5-9,4-,333d,-":               "#3-4,5-9,4-,333",
+					"#33-45,333":                       "#33-45,333",
+					"#35":                              "#35",
+					"#-2":                              "#-2",
+					"#1-4":                             "#1-4",
+					"#5-":                              "#5-",
+					"#5-10":                            "#5-10",
+					"#1,1-2,-1,4-,-":                   "#1,1-2,-1,4-,-",
+					"#1,-2,3-6,5-,-,dfhfjfdj":          "#1,-2,3-6,5-,-,",
+					"#3":                               "#3",
+					"#-":                               "#-",
+					"--changed-within 1week #3-4,5":    "#3-4,5",
+					"--changed-within 1week #3-4,5 dd": "#3-4,5",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := fields{}
+			if tt.prepare != nil {
+				tt.prepare(&f)
+			}
+
+			for k, v := range tt.args.n {
+				if got := f.c.FindAllString(k, -1); got[0] != v {
+					t.Errorf("FindAllString = %v, want %v", got, v)
+				}
+			}
+		})
+	}
+}
+
+func TestParseRangeNumber(t *testing.T) {
+
+	type res struct {
+		res []Range
+		err error
+	}
+
+	type fields struct {
+		c func(string, int) (*[]Range, error)
+	}
+
+	type args struct {
+		maxPage int
+		n       map[string]res
+	}
+
+	tests := []struct {
+		name    string
+		prepare func(f *fields)
+		args    args
+	}{
+		{
+			name: "test success",
+			prepare: func(f *fields) {
+				f.c = ParseRangeNumber
+			},
+			args: args{
+				n: map[string]res{
+					"3-4,5-9,-5,5-": {
+						res: []Range{
+							{Start: 3, End: 4},
+							{Start: 5, End: 9},
+							{Start: 1, End: 5},
+							{Start: 5, End: 10},
+						},
+						err: nil,
+					},
+				},
+				maxPage: 10,
+			},
+		},
+		{
+			name: "test fail with invalid page number",
+			prepare: func(f *fields) {
+				f.c = ParseRangeNumber
+			},
+			args: args{
+				n: map[string]res{
+					"3-4,20": {
+						res: []Range{},
+						err: fmt.Errorf("page range \"20\" is out of total page \"10\""),
+					},
+				},
+				maxPage: 10,
+			},
+		},
+		{
+			name: "test fail with start > end",
+			prepare: func(f *fields) {
+				f.c = ParseRangeNumber
+			},
+			args: args{
+				n: map[string]res{
+					"8-4": {
+						res: []Range{},
+						err: fmt.Errorf("page range start \"8\" must be less or equal to end \"4\""),
+					},
+				},
+				maxPage: 10,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := fields{}
+			if tt.prepare != nil {
+				tt.prepare(&f)
+			}
+
+			for k, v := range tt.args.n {
+				got, err := f.c(k, tt.args.maxPage)
+				if err != nil {
+					assert.Equal(t, v.err, err)
+				} else {
+					assert.Equal(t, v.res, *got)
+				}
+			}
+		})
+	}
+}
